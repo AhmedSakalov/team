a6d234011694eeadffa3c1a3fb0c9bdbf8c7174f
AhmedSakalov <56018472+AhmedSakalov@users.noreply.github.com>
2021-03-03 17:39:18 +0300

    main / functions

---
+++ functions.h
@@ -0,0 +1,25 @@
+int ft_plus(int a, int b)
+{
+    return (a + b);
+}
+
+int ft_minus(int a, int b)
+{
+    return (a - b);
+}
+
+int ft_div(int a, int b)
+{
+
+    return (a / b);
+}
+
+int ft_mod(int a, int b)
+{
+    return (a % b);
+}
+
+int ft_mult(int a, int b)
+{
+    return (a * b);
+}
